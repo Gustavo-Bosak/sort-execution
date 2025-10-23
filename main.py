2b85113f22296d37e0b33e838478a488031a1490
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-10-23 15:46:14 -0300

    :recycle: refactor: muda consumo para arquivo de texto

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ def limpar():
     else:
         os.system('clear')
 
-def voltar(lista):
+def voltar():
     print('\n------------------------------------------------')
     print('Voltando ao menu. Aperte qualquer tecla para continuar')
     input('')
-    menu(lista)
+    menu()
 
 def validarInteiroLimite(min, max):
     while True:
@@ -35,65 +35,64 @@ def criarlista():
 
     n = validarInteiroLimite(1, 900000)
     lista = [random.randint(0, 9999) for _ in range(n)]
+
+    with open('lista.txt', 'w') as file:
+        file.write(','.join(map(str, lista)))
     
     return lista
 
+def carregarLista():
+    if not os.path.exists('lista.txt'):
+        return []
+    
+    with open('lista.txt', 'r') as file:
+        conteudo = file.read().strip()
+        
+        if not conteudo:
+            return []
+        
+        return list(map(int, conteudo.split(',')))
+
 def imprimirRelatorio(lista, sort):
+    limpar()
     start = time.time()
     sortLista = sort(lista)
     stop = time.time()
 
-    print('\nLISTAS')
+    print('\nRESULTADOS DO SORT')
     print('------------------------------------------------')
     if len(lista)> 20:
-        print('- Lista original: ', lista[:10], '...', lista[-10:])
-        print('- Lista ordenada: ', sortLista[:10], '...', sortLista[-10:])
+        print('- Lista original: ', str(lista[:10]).replace(']', ''), '...', str(lista[-10:]).replace('[', ''))
+        print('- Lista ordenada: ', str(sortLista[:10]).replace(']', ''), '...', str(sortLista[-10:]).replace('[', ''))
+
     else:
         print('- Lista original: ', lista)
         print('- Lista ordenada: ', sortLista)
 
-    print(f'- Tempo de ordenação: {(stop - start):.2f} segundos')
-    voltar(lista)
+    print(f'- Tempo de ordenação: {(stop - start):.3f} segundos')
+    voltar()
 
 def imprimirRelCompleto(lista):
     limpar()
-
-    sorts = [
-        {
-            "nome": "bubble",
-            "metodo": bubbleSort,
-            "execucao": 0
-        },
-        {
-            "nome": "selection",
-            "metodo": selectionSort,
-            "execucao": 0
-        },
-        {
-            "nome": "insertion",
-            "metodo": insertionSort,
-            "execucao": 0
-        },
-        {
-            "nome": "merge",
-            "metodo": mergeSort,
-            "execucao": 0
-        }
-    ]
-
-    for i in range(4):
-        start = time.time()
-        sorts[i]["metodo"](lista)
-        stop = time.time()
-        sorts[i]["execucao"] = stop - start
-
     print('------------------------------------------------')
     print('RELATÓRIO DE SORTS')
     print('------------------------------------------------')
-    
-    for i in range(4):
-        print(f'Tempo de ordenação do {sorts[i]["nome"]} sort: {(sorts[i]["execucao"]):.2f} segundos')
-    voltar(lista)
+
+    sorts = [
+        ("bubble", bubbleSort ),
+        ("selection", selectionSort ),
+        ("insertion", insertionSort ),
+        ("merge", mergeSort ),
+    ]
+
+    for nome, metodo in sorts:
+        start = time.time()
+        metodo(lista)
+        stop = time.time()
+
+        print(f'Tempo de ordenação do {nome} sort: {(stop - start):3f} segundos')
+
+    voltar()
 
 def bubbleSort(lista):
     bubbleLista = lista.copy()
@@ -120,7 +119,7 @@ def mergeSort(lista):
     # as lógicas aqui, baseadas nessa lista copiada
     return mergeLista
 
-def menu(lista):
+def menu():
     limpar()
     print('------------------------------------------------')
     print('ALGORITMOS DE SORTS')
@@ -129,40 +128,26 @@ def menu(lista):
     print('1. Bubble Sort\n2. Selection Sort\n3. Insertion Sort\n4. Merge Sort\n5. Gerar relatório completo\n6. Definir nova lista\n7. Encerrar programa')
 
     escolha = validarInteiroLimite(1, 7)
+    lista = carregarLista()
+
     match escolha:
         case 1:
-            limpar()
-            print('------------------------------------------------')
-            print('BUBBLE SORT')
-            print('------------------------------------------------')
             imprimirRelatorio(lista, bubbleSort)
 
         case 2:
-            limpar()
-            print('------------------------------------------------')
-            print('SELECTION SORT')
-            print('------------------------------------------------')
             imprimirRelatorio(lista, selectionSort)
 
         case 3:
-            limpar()
-            print('------------------------------------------------')
-            print('INSERTION SORT')
-            print('------------------------------------------------')
             imprimirRelatorio(lista, insertionSort)
 
         case 4:
-            limpar()
-            print('------------------------------------------------')
-            print('MERGE SORT')
-            print('------------------------------------------------')
             imprimirRelatorio(lista, mergeSort)
 
         case 5:
             imprimirRelCompleto(lista)
 
         case 6:
-            main(lista)
+            main()
 
         case 7:
             print('------------------------------------------------')
@@ -170,32 +155,32 @@ def menu(lista):
             print('------------------------------------------------')
             return
 
-def main(lista = []):
+def main():
     limpar()
     print('------------------------------------------------')
     print('PROGRAMA DE TESTE DE SORTS')
     print('------------------------------------------------')
     print('Precisamos definir uma lista para testagem.')
-
-    if len(lista) > 0:
-        print('1. Gerar uma nova lista \n2. Continuar com a lista anterior\n3. Encerrar programa')
+    
+    listaArquivo = carregarLista()
+    if listaArquivo:
+        print('Foi encontrado um arquivo lista.txt com dados.')
+        print('1. Usar a lista existente\n2. Gerar uma nova lista\n3. Encerrar programa')
         escolha = validarInteiroLimite(1, 3)
-        
-        match escolha:
-            case 1:
-                listaSort = criarlista()
-            
-            case 2:
-                listaSort = lista
+        if escolha == 1:
+            pass
+        elif escolha == 2:
+            criarlista()
+        elif escolha == 3:
+            print('------------------------------------------------')
+            print('PROGRAMA FINALIZADO')
+            print('------------------------------------------------')
+            return
 
-            case 3:
-                print('------------------------------------------------')
-                print('PROGRAMA FINALIZADO')
-                print('------------------------------------------------')
-                return
     else:
-        listaSort = criarlista()
+        print('Nenhum arquivo lista.txt válido encontrado. Criando nova lista...')
+        criarlista()
 
-    menu(listaSort)
+    menu()
 
 main()
